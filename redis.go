287e691df0d49563881e1293f17c328d7b64cfef
Thang Bui <thangbx@onemount.com>
2021-08-07 08:23:54 +0700

    Refactor code

--- redis.go
+++ redis.go
@@ -3,14 +3,12 @@ package golibdata
 import (
 	"fmt"
 	red "github.com/go-redis/redis/v8"
-	"gitlab.id.vin/vincart/golib"
 	"gitlab.id.vin/vincart/golib-data/redis"
+	"gitlab.id.vin/vincart/golib/config"
 )
 
-func NewRedisAutoConfig(app *golib.App) *red.Client {
-	properties := &redis.Properties{}
-	app.ConfigLoader.Bind(properties)
-
+func NewRedisAutoConfig(loader config.Loader) *red.Client {
+	properties := redis.NewProperties(loader)
 	client, err := redis.NewClient(properties)
 	if err != nil {
 		panic(fmt.Sprintf("Cannot init redis with error [%s]", err.Error()))
--- redis/properties.go
+++ redis/properties.go
@@ -1,12 +1,21 @@
 package redis
 
-import _ "gitlab.id.vin/vincart/golib"
+import (
+	_ "gitlab.id.vin/vincart/golib"
+	"gitlab.id.vin/vincart/golib/config"
+)
 
 type Properties struct {
 	Host     string `default:"localhost"`
 	Port     int    `default:"6379"`
 	Database int    `default:"0"`
 	Password string
+}
+
+func NewProperties(loader config.Loader) *Properties {
+	props := Properties{}
+	loader.Bind(&props)
+	return &props
 }
 
 func (p Properties) Prefix() string {
