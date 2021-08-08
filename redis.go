821d61676c61165cd3440ee5d0de4d571baf9807
Thang Bui <thangbx@onemount.com>
2021-08-08 14:01:51 +0700

    Register health check for redis

--- redis.go
+++ redis.go
@@ -5,16 +5,27 @@ import (
 	red "github.com/go-redis/redis/v8"
 	"gitlab.id.vin/vincart/golib-data/redis"
 	"gitlab.id.vin/vincart/golib/config"
+	"gitlab.id.vin/vincart/golib/web/actuator"
+	"go.uber.org/fx"
 )
 
-func NewRedisAutoConfig(loader config.Loader) (*red.Client, error) {
+type RedisOut struct {
+	fx.Out
+	Client        *red.Client
+	HealthChecker actuator.HealthChecker `group:"actuator_health_checker"`
+}
+
+func NewRedisAutoConfig(loader config.Loader) (RedisOut, error) {
+	out := RedisOut{}
 	properties, err := redis.NewProperties(loader)
 	if err != nil {
-		return nil, err
+		return out, err
 	}
 	client, err := redis.NewClient(properties)
 	if err != nil {
-		return nil, fmt.Errorf("cannot init redis with error [%s]", err.Error())
+		return out, fmt.Errorf("cannot init redis with error [%s]", err.Error())
 	}
-	return client, nil
+	out.Client = client
+	out.HealthChecker = redis.NewHealthChecker(client)
+	return out, nil
 }
---
+++ redis/health.go
@@ -0,0 +1,33 @@
+package redis
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"gitlab.id.vin/vincart/golib/log"
+	"gitlab.id.vin/vincart/golib/web/actuator"
+)
+
+type HealthChecker struct {
+	client *redis.Client
+}
+
+func NewHealthChecker(client *redis.Client) actuator.HealthChecker {
+	return &HealthChecker{client: client}
+}
+
+func (h HealthChecker) Component() string {
+	return "redis"
+}
+
+func (h HealthChecker) Check() actuator.StatusDetails {
+	statusDetails := actuator.StatusDetails{
+		Status: actuator.StatusUp,
+	}
+	_, err := h.client.Ping(context.Background()).Result()
+	if err != nil {
+		log.Errorf("Redis health check failed, err [%s]", err.Error())
+		statusDetails.Status = actuator.StatusDown
+		statusDetails.Reason = "Redis health check failed"
+	}
+	return statusDetails
+}
