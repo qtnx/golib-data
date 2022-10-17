d8c490a47b8a389f7a0da7270bebcccd706a836a
namnd <namnd@technixo.com>
2022-10-17 18:05:44 +0700

    Expose test util functions from package

--- testutil/database_test_util.go
+++ testutil/database_test_util.go
@@ -4,52 +4,59 @@ import (
 	"fmt"
 	"github.com/stretchr/testify/require"
 	"gitlab.com/golibs-starter/golib/log"
+	"go.uber.org/fx"
 	"gorm.io/gorm"
 	"testing"
 )
 
-type DatabaseTestUtil struct {
-	db *gorm.DB
+var orm *gorm.DB
+
+func EnableDatabaseTestUtil() fx.Option {
+	return fx.Invoke(func(db *gorm.DB) {
+		orm = db
+	})
 }
 
-func NewDatabaseTestUtil(db *gorm.DB) *DatabaseTestUtil {
-	return &DatabaseTestUtil{db: db}
-}
-
-func (d *DatabaseTestUtil) TruncateTable(table string) {
-	if err := d.db.Exec(fmt.Sprintf("TRUNCATE TABLE `%s`", table)).Error; err != nil {
+func truncateTable(table string) {
+	if err := orm.Exec(fmt.Sprintf("TRUNCATE TABLE `%s`", table)).Error; err != nil {
 		log.Fatalf("Could not truncate table [%s], err [%v]", table, err)
 	} else {
 		log.Infof("Truncated table [%s]", table)
 	}
 }
 
-func (d *DatabaseTestUtil) Insert(model interface{}) {
-	if err := d.db.Create(model).Error; err != nil {
+func TruncateTables(tables ...string) {
+	for _, table := range tables {
+		truncateTable(table)
+	}
+}
+
+func Insert(model interface{}) {
+	if err := orm.Create(model).Error; err != nil {
 		log.Fatalf("Could not create seed data, model: [%+v], err: [%v]", model, err)
 	}
 }
 
-func (d *DatabaseTestUtil) CountWithoutQuery(table string) int64 {
+func CountWithoutQuery(table string) int64 {
 	var count int64
-	d.db.Table(table).Count(&count)
+	orm.Table(table).Count(&count)
 	return count
 }
 
-func (d *DatabaseTestUtil) CountWithQuery(table string, conditions map[string]interface{}) int64 {
+func CountWithQuery(table string, conditions map[string]interface{}) int64 {
 	var count int64
-	d.db.Table(table).Where(conditions).Count(&count)
+	orm.Table(table).Where(conditions).Count(&count)
 	return count
 }
 
 // AssertDatabaseCount assert database has number of row without query
-func (d *DatabaseTestUtil) AssertDatabaseCount(t *testing.T, table string, expected int64) {
-	count := d.CountWithoutQuery(table)
+func AssertDatabaseCount(t *testing.T, table string, expected int64) {
+	count := CountWithoutQuery(table)
 	require.Equal(t, expected, count)
 }
 
 // AssertDatabaseHas assert database has more than a row with query params
-func (d *DatabaseTestUtil) AssertDatabaseHas(t *testing.T, table string, conditions map[string]interface{}) {
-	count := d.CountWithQuery(table, conditions)
+func AssertDatabaseHas(t *testing.T, table string, conditions map[string]interface{}) {
+	count := CountWithQuery(table, conditions)
 	require.GreaterOrEqual(t, count, int64(1), "Record not found in database with query:", conditions)
 }
